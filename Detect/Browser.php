e8f6374d66ccb35690137153fb5a5a964ef37650
roelleor <r.bousardt@gmail.com>
2015-10-26 11:26:45 +0100

    first commit

---
+++ Detect/Browser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Wvs\BrowserBundle\Detect;
+
+class Browser extends \Browser
+{
+    const DEVICE_TYPE_DESKTOP = 'Desktop';
+    const DEVICE_TYPE_MOBILE = 'Mobile';
+    const DEVICE_TYPE_TABLET = 'Tablet';
+
+    public function getDeviceType()
+    {
+        if ($this->isMobile()) {
+            return self::DEVICE_TYPE_MOBILE;
+        } elseif ($this->isTablet()) {
+            return self::DEVICE_TYPE_TABLET;
+        }
+
+        return self::DEVICE_TYPE_DESKTOP;
+    }
+}
---
+++ WvsBrowserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wvs\BrowserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WvsBrowserBundle extends Bundle
+{
+}
